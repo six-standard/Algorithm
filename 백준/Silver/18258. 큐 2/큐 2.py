59c7e7a91cd09222c12ac409862f82f71f54a672
six-standard <dbrrl1127@gmail.com>
2023-07-07 08:30:07 +0900

    [Silver IV] Title: 큐 2, Time: 1756 ms, Memory: 175200 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+queue = deque()
+
+a = int(input())
+
+for i in range(a):
+    b = input().split()
+    if(b[0] == "push"):
+        queue.append(b[1])
+    if(b[0] == "pop"):
+        print(-1 if not queue else queue.popleft())
+    if(b[0] == "size"):
+        print(len(queue))
+    if(b[0] == "empty"):
+        print(0 if queue else 1)
+    if(b[0] == "front"):
+        print(queue[0] if queue else -1)
+    if(b[0] == "back"):
+        print(queue[-1] if queue else -1)
