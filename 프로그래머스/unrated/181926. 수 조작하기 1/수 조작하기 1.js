3860a9ca4a0df43c090955032e54ce5092cff24e
six-standard <dbrrl1127@gmail.com>
2023-09-11 19:17:11 +0900

    [unrated] Title: 수 조작하기 1, Time: 2.26 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,20 @@
+function solution(n, control) {
+    control = control.split('');
+    control.map(item => {
+        switch(item) {
+            case "w":
+                n = n+1;
+                break;
+            case "s":
+                n = n-1;
+                break
+            case "d":
+                n = n+10;
+                break
+            case "a":
+                n = n-10;
+                break;
+        }
+    });
+    return n;
+}
