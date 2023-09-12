ad307951cfbd9ac12c96304c165897fab87430ee
six-standard <dbrrl1127@gmail.com>
2023-09-12 21:35:23 +0900

    [unrated] Title: l로 만들기, Time: 3.79 ms, Memory: 34.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
@@ -0,0 +1,15 @@
+function solution(myString) {
+    return myString
+        .replaceAll('a', 'l')
+        .replaceAll('b', 'l')
+        .replaceAll('c', 'l')
+        .replaceAll('d', 'l')
+        .replaceAll('e', 'l')
+        .replaceAll('f', 'l')
+        .replaceAll('g', 'l')
+        .replaceAll('h', 'l')
+        .replaceAll('i', 'l')
+        .replaceAll('j', 'l')
+        .replaceAll('k', 'l')
+    ;
+}
