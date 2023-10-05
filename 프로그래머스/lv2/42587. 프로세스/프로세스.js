8d8ea4ba199aa090521b1b23f093515fb229353c
six-standard <dbrrl1127@gmail.com>
2023-10-05 17:42:54 +0900

    [level 2] Title: 프로세스, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.js
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.js
@@ -1,17 +1,23 @@
+/*
+    2 -> Q
+    1 -> Q
+    3 -> (첫번째 실행)
+    
+*/
+
 function solution(priorities, location) {
-    let arr = [];
-    let max = Math.max(...priorities);
     let cnt = 0;
-    for(let i = 0; i<priorities.length; i++) arr.push([priorities[i], i]);
-    while(arr.length>0) {
-        let tmp = arr.shift();
-        if(tmp[0]<max) arr.push(tmp);
-        else {
+    let loc = location;
+    let max = Math.max(...priorities);
+    while(priorities.length>0) {
+        let data = priorities.shift();
+        if(data<max) {
             cnt++;
-            if(Number(tmp[1])===location) return(cnt);
-            tmp = [];
-            arr.forEach(items => tmp.push(items[0]))
-            max = Math.max(...tmp);
+            priorities.push(data);
+            if(loc!==0) loc--;
+            else loc = priorities.length-1;
+        } else {
+            if(loc===0) return cnt;
         }
     }
 }
