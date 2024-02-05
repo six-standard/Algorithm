561c09db3b0aabc22541cfc8d0d25b46b0e57a15
육기준 <dbrrl1127@gmail.com>
2024-02-05 23:58:55 +0900

    [Bronze V] Title: 콜센터, Time: 116 ms, Memory: 9308 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.js
@@ -0,0 +1,10 @@
+console.log("     /~\\");
+console.log("    ( oo|");
+console.log("    _\\=/_");
+console.log("   /  _  \\");
+console.log("  //|/.\\|\\\\");
+console.log(" ||  \\ /  ||");
+console.log("============");
+console.log("|          |");
+console.log("|          |");
+console.log("|          |");
