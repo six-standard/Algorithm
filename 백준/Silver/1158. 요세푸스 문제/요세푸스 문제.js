d6ced25a8b73a573a492edd255ca8a0dc5df0abc
육기준 <dbrrl1127@gmail.com>
2024-02-25 00:48:58 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 2572 ms, Memory: 18164 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
@@ -0,0 +1,23 @@
+// 코드 이해해서 다시 작성해보자
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const queue = Array.from(new Array(input[0]).keys()).map((i) => i + 1);
+let answer = [];
+
+let count = 1;
+while (queue.length) {
+  const shiftItem = queue.shift();
+  if (count % input[1] === 0) {
+    answer.push(shiftItem);
+  } else {
+    queue.push(shiftItem);
+  }
+  count += 1;
+}
+
+console.log(`<${answer.join(", ")}>`);
