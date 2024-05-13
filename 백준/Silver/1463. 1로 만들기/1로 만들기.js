7863995007551f9c620b8337c4737134e18f832b
육기준 <dbrrl1127@gmail.com>
2024-05-13 22:49:08 +0900

    [Silver III] Title: 1로 만들기, Time: 244 ms, Memory: 42684 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,18 @@
+const input = parseInt(
+  require("fs").readFileSync("/dev/stdin").toString().trim()
+);
+
+let dp = [0, 0];
+
+for (let i = 2; i <= input; i++) {
+  dp[i] = dp[i - 1] + 1;
+
+  if (i % 2 === 0) {
+    dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+  }
+  if (i % 3 === 0) {
+    dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+  }
+}
+
+console.log(dp[input]);
