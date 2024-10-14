37d8ded9060d7ac41aabd16d4ca77e7ea9133602
육기준 <dbrrl1127@gmail.com>
2024-10-14 20:27:53 +0900

    [Silver III] Title: 2×n 타일링, Time: 144 ms, Memory: 9648 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -1,16 +1,9 @@
-const input = Number(require('fs').readFileSync('/dev/stdin').toString().trim());;;
+const n = Number(require("fs").readFileSync("/dev/stdin").toString().trim());
 
-const solution = (n) => {
-  let dp = Array(n).fill(0);
+let arr = [1, 2];
 
-  dp[1] = 1;
-  dp[2] = 2;
-
-  for(let i = 3; i<=n; i++) {
-    dp[i] = (dp[i-1] + dp[i-2]) % 10007;
-  }
-
-  return(dp[n]);
+for (let i = 2; i < n; i++) {
+  arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
 }
 
-console.log(solution(input));
+console.log(arr[n - 1]);
