1fa81128d5e0fcebc149efc0d55ec0d9c922c1fd
육기준 <dbrrl1127@gmail.com>
2024-10-15 10:47:43 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 92 ms, Memory: 9372 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -1,11 +1,10 @@
-let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-input.shift();
+const [_, ...n] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
 let arr = [1, 2, 4];
 
-for (let i = 3; i <= Math.max.apply(null, input); i++) {
-  arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
-}
-
-input.forEach((i) => {
+for (let i of n) {
+  for (let j = 3; j < i; j++) {
+    arr[j] = arr[j - 1] + arr[j - 2] + arr[j - 3];
+  }
   console.log(arr[i - 1]);
-});
+}
