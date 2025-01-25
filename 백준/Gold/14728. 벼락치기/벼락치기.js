2877e6b78841e2707310499f9f3626d2db613faf
육기준 <dbrrl1127@gmail.com>
2025-01-26 00:03:43 +0900

    [Gold V] Title: 벼락치기, Time: 268 ms, Memory: 24496 KB -BaekjoonHub

---
+++ 백준/Gold/14728. 벼락치기/벼락치기.js
@@ -0,0 +1,20 @@
+console.log(solution(require("fs").readFileSync("/dev/stdin").toString().trim()));
+
+function solution(input) {
+  let [[length, max], ...items] = input.split("\n").map((i) => i.split(" ").map(Number));
+  [length, max] = [length + 1, max + 1];
+
+  const dp = Array.from({ length }, () => Array.from({ length: max }, () => 0));
+
+  for (let i = 1; i < length; i++) {
+    for (let j = 1; j < max; j++) {
+      if (j >= items[i - 1][0]) {
+        dp[i][j] = Math.max(dp[i - 1][j], items[i - 1][1] + dp[i - 1][j - items[i - 1][0]]);
+      } else {
+        dp[i][j] = dp[i - 1][j];
+      }
+    }
+  }
+
+  return dp[length - 1][max - 1];
+}
