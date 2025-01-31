a0ea1a511a438a2c063ef8b48d7197ebc756825c
육기준 <dbrrl1127@gmail.com>
2025-01-31 23:17:23 +0900

    [Silver II] Title: 점프 점프, Time: 164 ms, Memory: 10860 KB -BaekjoonHub

---
+++ 백준/Silver/11060. 점프 점프/점프 점프.js
@@ -0,0 +1,17 @@
+console.log(solution(require("fs").readFileSync("/dev/stdin").toString().trim()));
+
+function solution(input) {
+  const [[length], items] = input.split("\n").map((i) => i.split(" ").map(Number));
+  let dp = new Array(length).fill(Infinity);
+  dp[0] = 0;
+
+  for (let i = 0; i < length; i++) {
+    for (let j = i + 1; j <= i + items[i] && j < length; j++) {
+      if (dp[j] === Infinity) dp[j] = dp[i] + 1;
+      else dp[j] = Math.min(dp[j], dp[i] + 1);
+    }
+  }
+
+  if (dp[length - 1] === Infinity) return -1;
+  else return dp[length - 1];
+}
