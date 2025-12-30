614e47bd394cfd770378fec8589f974424b9a2b1
육기준 <github@stdd.dev>
2025-12-30 18:57:42 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#include <iostream>
+#include <ctime>
+using namespace std;
 
-int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d", a+b);
+int main(){
+    int a,b;
+    cin >> a >> b;
+    cout << a+b << "\n";
     return 0;
 }
