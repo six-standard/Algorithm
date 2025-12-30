ec6dda0ea78d58dcdd49bb76e0931f7ba12f3217
육기준 <github@stdd.dev>
2025-12-30 20:17:04 +0900

    [Bronze V] Title: N 찍기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int a;
+    cin >> a;
+    for(int b=1; b<=a; b++) {
+        cout << b << "\n";
+    }
+    
+    return 0;
+}
