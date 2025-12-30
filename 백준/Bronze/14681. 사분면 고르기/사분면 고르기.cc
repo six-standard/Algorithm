08607030b3796526b28844a18c4c924cd243a149
육기준 <github@stdd.dev>
2025-12-30 19:49:34 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+    cin >> a;
+    cin >> b;
+
+    if(a>0) {
+        if(b>0) { cout << 1; }
+        else { cout << 4; }
+    } else {
+        if(b>0) { cout << 2; }
+        else { cout << 3; }
+    }
+	return 0;
+}
