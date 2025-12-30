0e9b9eedfb1f87148fe0cfc4899da03b1d90b0c2
육기준 <github@stdd.dev>
2025-12-30 21:47:27 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    
+    long long a,b;
+    cin >> a >> b;
+    cout << (a+b)*(a-b);
+    
+    return 0;
+}
