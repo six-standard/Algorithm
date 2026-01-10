2e772146c53fbd3a6b353340a36e8840ae8e75cd
육기준 <github@stdd.dev>
2026-01-10 15:44:27 +0900

    [Bronze I] Title: 팰린드롬수, Time: 96 ms, Memory: 9428 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
@@ -1,23 +1,18 @@
-console.log(solution(require("fs").readFileSync("/dev/stdin").toString().trim()));
+const items = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((i) => i.split(""));
 
-function solution(input) {
-  const items = input.split("\n");
-  let ans = [];
-
-  for (let i = 0; i < items.length - 1; i++) {
-    const odd = items[i].length % 2;
-    let len = Math.floor(items[i].length / 2);
-    let [start, end] = [
-      items[i].slice(0, len),
-      items[i]
-        .slice(len + odd, items.length + 1)
-        .split("")
-        .reverse()
-        .join(""),
-    ];
-
-    ans.push(start === end ? "yes" : "no");
+items.forEach((item) => {
+  if (item[0] == "0") return;
+  let ck = true;
+  for (let i = Math.floor(item.length / 2) - 1; i >= 0; i--) {
+    if (item[i] !== item[item.length - 1 - i]) {
+      ck = false;
+      break;
+    }
   }
-
-  return ans.join("\n");
-}
+  console.log(ck ? "yes" : "no");
+});
