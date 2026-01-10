61ac3d48ddae3d3f24f89c436b8688aaf076131b
육기준 <github@stdd.dev>
2026-01-10 16:39:26 +0900

    [Silver V] Title: 단어 정렬, Time: 224 ms, Memory: 21244 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -1,3 +1,16 @@
-let [_, ...n] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+console.log(
+  solution(require("fs").readFileSync("/dev/stdin").toString().trim())
+);
 
-console.log([...new Set(n)].sort((i, j) => i.length - j.length || i.localeCompare(j)).join("\n"));
+function solution(input) {
+  const [_, ...items] = [...new Set(input.split("\n"))];
+
+  return items
+    .sort((a, b) => {
+      if (a.length !== b.length) {
+        return a.length - b.length;
+      }
+      return a.localeCompare(b);
+    })
+    .join("\n");
+}
